aec39eb7913fcad49ac4a25fc23f8a4e14e69eda
FabioDeMiranda <FabioDeMiranda@users.noreply.github.com>
2024-12-05 22:30:32 +0100

    updating

--- 03.sh
+++ 03.sh
@@ -55,7 +55,7 @@ fi
 
 # Merge Models, vae, lora, and hypernetworks, and outputs
 # Ignore move errors if they occur
-#sl_folder ${SD03_DIR}/invokeai models ${BASE_DIR}/models invokeai
+sl_folder ${SD03_DIR}/invokeai models ${BASE_DIR} models-invokeai
 
 # launch WebUI
 invokeai-web --config ${SD03_DIR}/config.yaml
