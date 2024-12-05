45120dd3140d7adc87c89132c7ff1bf6f0192995
FabioDeMiranda <FabioDeMiranda@users.noreply.github.com>
2024-12-05 19:25:06 +0100

    updating

--- 05.sh
+++ 05.sh
@@ -87,7 +87,7 @@ sl_folder ${SD05_DIR}/ComfyUI/models/ultralytics segm ${BASE_DIR}/models detecto
 
 sl_folder ${SD05_DIR}/ComfyUI/custom_nodes/IPAdapter-ComfyUI models ${BASE_DIR}/models controlnet
 
-ln -s "${SD05_DIR}/models/unet/" "${BASE_DIR}/models/checkpoints/"
+ln -s "${SD05_DIR}/ComfyUI/models/unet/" "${BASE_DIR}/models/checkpoints/"
 
 #install requirements
 cd ${SD05_DIR}/ComfyUI
